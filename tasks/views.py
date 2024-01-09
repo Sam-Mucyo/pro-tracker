ad01c6d97e65db28db7665289fa46786f3bfe271
Sam-Mucyo <90157786+Sam-Mucyo@users.noreply.github.com>
2024-01-09 15:47:30 -0700

    configure backend to return a fixed list of tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    tasks = ["Task 1", "Task 2", "Task 3"]
+
+    return render(request, "tasks/index.html", {"tasks": tasks})
